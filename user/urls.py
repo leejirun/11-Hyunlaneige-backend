66753a24e0056aea1eb27eb6c4c5626507d70bf2
chang sik kwon <kcs1987@gamil.com>
2020-09-07 12:37:40 +0900

    user app kakao login 피드백반영

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls    import path
 
-from .views         import SignupView, SigninView
+from .views         import SignupView, SigninView, KakaoSigninView, KakaoSignInCallbackView
 
 urlpatterns = [
     path('/signup', SignupView.as_view()),
     path('/signin', SigninView.as_view()),   
+    path('/signin/kakao', KakaoSigninView.as_view()),
+    path('/signin/kakao/callback', KakaoSignInCallbackView.as_view()),
 ]
